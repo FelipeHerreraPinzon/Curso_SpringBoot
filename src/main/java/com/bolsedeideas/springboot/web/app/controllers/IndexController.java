cac6cdd44e0ad86e6afae2940c035c0142e14259
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2023-11-29 06:10:22 -0500

    directiva if

--- src/main/java/com/bolsedeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsedeideas/springboot/web/app/controllers/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -20,12 +22,23 @@ public class IndexController {
 
     @RequestMapping("/perfil")
     public String perfil(Model model) {
+
         Usuario usuario = new Usuario();
         usuario.setNombre("Felipe");
         usuario.setApellido("Herrera");
+        usuario.setEmail("felipe@gmail.com");
+
         model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Perfil usuario ".concat(usuario.getNombre()));
         return "perfil";
+    }
+
+    @RequestMapping("/listar")
+    public String listar (Model model){
+        List<Usuario> usuarios = new ArrayList<>();
+        model.addAttribute("titulo", "Listado de usuarios ");
+        model.addAttribute("usuarios", usuarios);
+        return "listar";
     }
 
 
